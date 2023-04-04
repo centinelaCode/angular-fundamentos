b86b659a24a73a18995f18ff97d19e9b1b7bcf23
centinela1024tb <centinela1024tb@gmail.com>
2023-04-04 14:02:28 -0600

    [ADD] Estructura de control *ngFor para datos primitivos y arrays de objetos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { Product } from './product.model'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -17,6 +19,37 @@ export class AppComponent {
     age: 18,
     avatar: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRp0xKoXUryp0JZ1Sxp-99eQiQcFrmA1M1qbQ&usqp=CAU'
   }
+  names: string[] = ['Nico', 'Juli', 'Santi'];
+  newName = '';
+  products: Product[] = [
+    {
+      name: 'Consola Nintendo',
+      price: 1200,
+      image: './assets/images/nintendo.jpg',
+      category: 'Consolas',
+    },
+    {
+      name: 'Consola Super Nintendo',
+      price: 1500,
+      image: './assets/images/supernintendo.jpg'
+    },
+    {
+      name: 'Consola Nintendo 64',
+      price: 800,
+      image: './assets/images/nintendo64.jpg'
+    },
+    {
+      name: 'Game Cube Nintendo',
+      price: 900,
+      image: './assets/images/gamecube.jpg'
+    },
+    {
+      name: 'Game Cube Nintendo',
+      price: 1100,
+      image: './assets/images/wii.jpg'
+    },
+  ]
+
 
 
 
@@ -25,18 +58,33 @@ export class AppComponent {
   toggleButton() {
     this.btnDesable = !this.btnDesable;
   }
+
   increaseAge() {
     this.person.age += 1;
   }
+
   dicreaseAge() {
     this.person.age -= 1;
   }
+
   onScroll(event: Event) {
     const element = event.target as HTMLElement;
     console.log(element.scrollTop)
   }
+
   changeName(event: Event){
     const element = event.target as HTMLInputElement;
     this.person.name = element.value;
+  }
+
+  addName() {
+    if (this.newName !== '') {
+      this.names.push(this.newName);
+      this.newName = '';
+    }
+  }
+
+  deleteName(index: number) {
+    this.names.splice(index, 1);
   }
 }
